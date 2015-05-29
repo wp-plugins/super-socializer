a86b03fad10f35ebee2941107f2b4d840a472840
the_champ <the_champ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 06:59:09 +0000

    Minor change
    git-svn-id: https://plugins.svn.wordpress.org/super-socializer/trunk@1169913 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/front/facebook/commenting.js
+++ js/front/facebook/commenting.js
@@ -1,1 +1,64 @@
-function theChampRenderFBCommenting(){if(!document.getElementById("disqus_thread")){var e=document.getElementById("respond"),t=[],a=[],m=[];e&&(t.wordpress='<div style="clear:both"></div>'+e.innerHTML),theChampFBCommentingContent='<div class="fb-comments" data-href="'+theChampFBCommentUrl+'"',""!=theChampFBCommentColor&&(theChampFBCommentingContent+=' data-colorscheme="'+theChampFBCommentColor+'"'),""!=theChampFBCommentNumPosts&&(theChampFBCommentingContent+=' data-numposts="'+theChampFBCommentNumPosts+'"'),theChampFBCommentingContent+=' data-width="'+theChampFBCommentWidth+'"',""!=theChampFBCommentOrderby&&(theChampFBCommentingContent+=' data-order-by="'+theChampFBCommentOrderby+'"'),""!=theChampFBCommentMobile&&(theChampFBCommentingContent+=' data-mobile="'+theChampFBCommentMobile+'"'),theChampFBCommentingContent+=" ></div>",t.fb=theChampFBCommentingContent,a.fb="theChampInitiateFB();",t.googleplus="<div class='g-comments' data-href='"+theChampGpCommentsUrl+"' "+(theChampGpCommentsWidth?"data-width='"+theChampGpCommentsWidth+"'":"")+" data-first_party_property='BLOGGER' data-view_type='FILTERED_POSTMOD' ></div>",a.googleplus=" ",m.googleplus="//apis.google.com/js/plusone.js",t.disqus='<div class="embed-container clearfix" id="disqus_thread">'+(""!=theChampDisqusShortname?theChampDisqusShortname:'<div style="font-size: 14px;clear: both;">Specify a Disqus shortname in Super Socializer &gt; Social Commenting section in admin panel</div>')+"</div>",a.disqus="var disqus_shortname = '"+theChampDisqusShortname+"';(function(d) {var dsq = d.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(dsq); })(document);";var n='<div class="theChampCommentingTabs"><h3 id="theChampReplyTitle" style="margin-bottom:15px" class="comment-reply-title">'+theChampScLabel+"</h3><ul>";e||(theChampScEnabledTabs=theChampScEnabledTabs.replace("wordpress,","").replace("wordpress","")),theChampScEnabledTabs=theChampScEnabledTabs.split(",");for(var h=0;h<theChampScEnabledTabs.length;h++){n+='<li id="theChampTabs-'+h+'-li" onclick="',n+="this.setAttribute('class', 'theChampSelectedTab');document.getElementById('theChampTabs-"+h+"').style.display='block';";for(var s=0;s<theChampScEnabledTabs.length;s++)s!=h&&(n+="document.getElementById('theChampTabs-"+s+"-li').setAttribute('class', '');document.getElementById('theChampTabs-"+s+"').style.display='none';");n+='">',n+=theChampScTabLabels[theChampScEnabledTabs[h]],n+="</li>"}n+="</ul>";for(var h=0;h<theChampScEnabledTabs.length;h++)n+='<div id="theChampTabs-'+h+'"><div style="clear: both"></div>'+t[theChampScEnabledTabs[h]]+"</div>";if(n+="</div>",e)e.innerHTML=n,document.getElementById("reply-title").remove();else{if(document.getElementsByTagName("article").length)var d=document.getElementsByTagName("article")[0];else{if(!document.getElementById("content"))return;var d=document.getElementById("content")}var o=document.createElement("div");o.setAttribute("id","comments"),o.setAttribute("class","comments-area"),o.innerHTML="<div id='respond' class='comment-respond'>"+n+"</div>",d.parentNode.insertBefore(o,d.nextSibling)}for(var h=0;h<theChampScEnabledTabs.length;h++)if(a[theChampScEnabledTabs[h]]){var i=document.createElement("script");m[theChampScEnabledTabs[h]]&&i.setAttribute("src",m[theChampScEnabledTabs[h]]),i.innerHTML=a[theChampScEnabledTabs[h]],document.getElementById("theChampTabs-"+h).appendChild(i)}document.getElementById("theChampTabs-0-li").setAttribute("class","theChampSelectedTab"),setTimeout(function(){for(var e=1;e<theChampScEnabledTabs.length;e++)document.getElementById("theChampTabs-"+e).style.display="none"},3000)}}theChampLoadEvent(function(){theChampRenderFBCommenting()});
+function theChampRenderFBCommenting() {
+    var t = document.getElementById("respond");
+    if(!t){
+        return;
+    }
+    var htmls = [];
+    var scripts = [];
+    var scriptsSrc = [];
+    // wordpress
+    htmls['wordpress'] = '<div style="clear:both"></div>' + t.innerHTML;
+    // facebook
+    theChampFBCommentingContent = '<div class="fb-comments" data-href="' + theChampFBCommentUrl + '"', "" != theChampFBCommentColor && (theChampFBCommentingContent += ' data-colorscheme="' + theChampFBCommentColor + '"'), "" != theChampFBCommentNumPosts && (theChampFBCommentingContent += ' data-numposts="' + theChampFBCommentNumPosts + '"'), theChampFBCommentingContent += ' data-width="' + theChampFBCommentWidth + '"', "" != theChampFBCommentOrderby && (theChampFBCommentingContent += ' data-order-by="' + theChampFBCommentOrderby + '"'), "" != theChampFBCommentMobile && (theChampFBCommentingContent += ' data-mobile="' + theChampFBCommentMobile + '"'), theChampFBCommentingContent += " ></div>";
+    htmls['fb'] = theChampFBCommentingContent;
+    scripts['fb'] = 'theChampInitiateFB();';
+    
+    // googleplus
+    htmls['googleplus'] = "<div class='g-comments' data-href='" + theChampGpCommentsUrl + "' "+ (theChampGpCommentsWidth ? "data-width='"+ theChampGpCommentsWidth +"'" : "") +" data-first_party_property='BLOGGER' data-view_type='FILTERED_POSTMOD' ></div>";
+    scripts['googleplus'] = ' ';
+    scriptsSrc['googleplus'] = '//apis.google.com/js/plusone.js';
+
+    // disqus
+    htmls['disqus'] = '<div class="embed-container clearfix" id="disqus_thread">' + (theChampDisqusShortname != '' ? theChampDisqusShortname : '<div style="font-size: 14px;clear: both;">Specify a Disqus shortname in Super Socializer &gt; Social Commenting section in admin panel</div>') + '</div>';
+    scripts['disqus'] = "var disqus_shortname = '" + theChampDisqusShortname + "';(function(d) {var dsq = d.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(dsq); })(document);";
+    var commentingHtml = '<div class="theChampCommentingTabs"><h3 id="theChampReplyTitle" style="margin-bottom:15px" class="comment-reply-title">'+ theChampScLabel +'</h3><ul>';
+    theChampScEnabledTabs = theChampScEnabledTabs.split(',');
+    for(var i = 0; i < theChampScEnabledTabs.length; i++){
+        commentingHtml += '<li id="theChampTabs-'+ i +'-li" onclick="';
+        commentingHtml += 'this.setAttribute(\'class\', \'theChampSelectedTab\');document.getElementById(\'theChampTabs-'+ i +'\').style.display=\'block\';';
+        for(var j = 0; j < theChampScEnabledTabs.length; j++){
+            if(j != i){
+                commentingHtml += 'document.getElementById(\'theChampTabs-' + j + '-li\').setAttribute(\'class\', \'\');document.getElementById(\'theChampTabs-'+ j +'\').style.display=\'none\';';
+            }
+        }
+        commentingHtml += '">';
+        commentingHtml += theChampScTabLabels[theChampScEnabledTabs[i]];
+        commentingHtml += '</li>';
+    }
+    commentingHtml += '</ul>';
+    for(var i = 0; i < theChampScEnabledTabs.length; i++){
+        commentingHtml += '<div id="theChampTabs-' + i + '"><div style="clear: both"></div>' + htmls[theChampScEnabledTabs[i]] + '</div>';
+    }
+    commentingHtml += '</div>';
+    t.innerHTML = commentingHtml;
+    document.getElementById("reply-title").remove();
+    for(var i = 0; i < theChampScEnabledTabs.length; i++){
+        if(scripts[theChampScEnabledTabs[i]]){
+            var script = document.createElement('script');
+            if(scriptsSrc[theChampScEnabledTabs[i]]){
+                script.setAttribute('src', scriptsSrc[theChampScEnabledTabs[i]]);
+            }
+            script.innerHTML = scripts[theChampScEnabledTabs[i]];
+            document.getElementById('theChampTabs-' + i).appendChild(script);
+        }
+    }
+    document.getElementById('theChampTabs-0-li').setAttribute('class', 'theChampSelectedTab');
+    setTimeout(function(){
+        for(var i = 1; i < theChampScEnabledTabs.length; i++){
+           document.getElementById('theChampTabs-' + i).style.display = 'none';
+        }
+    }, 3000);
+}
+theChampLoadEvent(function() {
+    theChampRenderFBCommenting()
+});
